37128f1a6c92d1b916327bb12165d374005f3b8f
elbouchti-khaoula <83877800+elbouchti-khaoula@users.noreply.github.com>
2022-05-27 13:19:15 +0100

    Update LoginActivity.java

--- HOMEFORPET/app/src/main/java/com/example/home_for_pet/LoginActivity.java
+++ HOMEFORPET/app/src/main/java/com/example/home_for_pet/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -28,7 +29,7 @@ public class LoginActivity extends AppCompatActivity{
         auth=FirebaseAuth.getInstance();
         email=findViewById(R.id.email);
         password=findViewById(R.id.password);
-        ImageButton create = (ImageButton)findViewById(R.id.sign_up_on_sign_up);
+        Button create = (Button)findViewById(R.id.sign_in_on_sign_in);
         create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
