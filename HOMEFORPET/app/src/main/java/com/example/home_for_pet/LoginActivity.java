2d4ad067db7cdbd421ade7ccbc537880c04e6498
elbouchti-khaoula <83877800+elbouchti-khaoula@users.noreply.github.com>
2022-05-27 12:45:26 +0100

    Add files via upload

---
+++ HOMEFORPET/app/src/main/java/com/example/home_for_pet/LoginActivity.java
@@ -0,0 +1,72 @@
+package com.example.home_for_pet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity{
+    EditText email,password;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+        getSupportActionBar().hide();
+        auth=FirebaseAuth.getInstance();
+        email=findViewById(R.id.email);
+        password=findViewById(R.id.password);
+        ImageButton create = (ImageButton)findViewById(R.id.sign_up_on_sign_up);
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegistrationActivity.class));
+
+            }
+        });
+    }
+
+    public void signIn(View view){
+        String userEmail= email.getText().toString();
+        String userPassword= password.getText().toString();
+        if(TextUtils.isEmpty(userEmail)){
+            Toast.makeText(this, "Entrer l'e mail", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(userPassword)){
+            Toast.makeText(this, "Entrer le password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(userPassword.length()<8){
+            Toast.makeText(this, "le mdp est too short, il faut au minimum 8 char", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        auth.signInWithEmailAndPassword(userEmail,userPassword)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Login Successful" ,Toast.LENGTH_SHORT );
+                            startActivity(new Intent(LoginActivity.this , MainActivity.class));
+                        }else{
+                            Toast.makeText(LoginActivity.this , "Error"+task.getException() , Toast.LENGTH_SHORT);
+                        }
+                    }});
+    }
+    public void singup(android.view.View view) {
+        startActivity(new Intent(LoginActivity.this, MainActivity.class));}
+
+
+}
