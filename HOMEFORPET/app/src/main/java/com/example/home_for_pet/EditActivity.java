4878d916761e54b6695648dc20200f97e8eed544
salmaelmi <76711482+salmaelmi@users.noreply.github.com>
2022-05-28 05:14:00 +0100

    e

---
+++ HOMEFORPET/app/src/main/java/com/example/home_for_pet/EditActivity.java
@@ -0,0 +1,79 @@
+package com.example.demofirebasetorecycler;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EditActivity extends AppCompatActivity
+{
+    EditText name,category,location;
+    Button add,back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adddata);
+
+        name=(EditText)findViewById(R.id.add_name);
+        category=(EditText)findViewById(R.id.add_category);
+        location=(EditText)findViewById(R.id.add_location);
+
+        back=(Button)findViewById(R.id.add_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        });
+
+        add=(Button)findViewById(R.id.add_submit);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                processinsert();
+            }
+        });
+    }
+
+    private void processinsert()
+    {
+        Map<String,Object> map=new HashMap<>();
+        map.put("name",name.getText().toString());
+        map.put("category",category.getText().toString());
+        map.put("location",location.getText().toString());
+        FirebaseDatabase.getInstance().getReference().child("students").push()
+                .setValue(map)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        name.setText("");
+                        category.setText("");
+                        location.setText("");
+                        Toast.makeText(getApplicationContext(),"Inserted Successfully",Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e)
+                    {
+                        Toast.makeText(getApplicationContext(),"Could not insert",Toast.LENGTH_LONG).show();
+                    }
+                });
+
+    }
+}
